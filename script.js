4258942b9fc7e3bc3859225d5306a8da0b11ed37
LadoGelava910 <76868669+LadoGelava910@users.noreply.github.com>
2022-02-11 17:31:00 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -21,11 +21,36 @@ const swiper = new Swiper(".swiper", {
 });
 
 function close() {
-  document.getElementById("swiper").style.display = "none";
+  document.getElementById("swiper_wrapper").style.display = "none";
 }
 
-document.getElementsByClassName("close_pop_up").map((item) => {
-  return (item.onclick = function () {
-    alert("button was clicked");
-  });
+// Selecting elements.
+const container = document.querySelector(".container");
+const h1 = document.querySelector(".container h1");
+// The clock function.
+const clock = () => {
+  //   Accessing the date object.
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let secondes = date.getSeconds();
+  //   Adding a zero to the left of the time if it's less or equal   than 9.
+  if (+hours <= 9) {
+    hours = "0" + hours;
+  } else if (+minutes <= 9) {
+    minutes = "0" + minutes;
+  } else if (+secondes <= 9) {
+    secondes = "0" + secondes;
+  }
+  // adding the time to the h1 element.
+  h1.innerHTML = hours + ":" + minutes;
+
+  //Toggling the animate class.
+  container.classList.toggle("animate");
+};
+// calling the clock function after each second(1000ms).
+setInterval(clock, 1000);
+
+document.getElementById("button_close").addEventListener("click", function () {
+  close();
 });
